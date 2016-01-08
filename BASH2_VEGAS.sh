4a15d528886d34b2addeec196924c6bc5d9f3777
Eric Moyer <eric_moyer@yahoo.com>
2016-01-07 22:38:46 -0500

    Add command-line arguments to vegas script
    I also made it accessible by absolute path

--- BASH2_VEGAS.sh
+++ BASH2_VEGAS.sh
@@ -1,9 +1,6 @@
+#!/bin/bash
+
 #Converting SNP Association data to a gene-level summary using VEGAS:
 
-#Change directory to the folder Vegas
-cd VEGAS
-
 #Using Vegas to Convert a full tab-delimited text file containing 2 columns of data: SNP names and p-values (no header! no quotes!) to a gene level summary of p-values:
-./vegas ../TestData_OnlySNPs_pvalues.txt -pop hapmapCEU -out TestData_Gene_pvalues
-
-cd ../
+/home/ubuntu/VEGAS/vegas $1 -pop hapmapCEU -out $2
