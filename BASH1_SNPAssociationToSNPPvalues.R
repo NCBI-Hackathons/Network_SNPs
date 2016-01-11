485e2c4a237ebd16c1135224de91411f8c970716
Megan Hagenauer <hagenaue@umich.edu>
2016-01-10 21:49:06 -0500

    Update BASH1_SNPAssociationToSNPPvalues.R
    Hi folks,
    Sorry, I really don't know how to use github, so I just overwrote my earlier script with the newer version (now actually functional).
    The primary changes were in the argument line and description accessed by help.
    Cheers,
    Megan

--- BASH1_SNPAssociationToSNPPvalues.R
+++ BASH1_SNPAssociationToSNPPvalues.R
@@ -4,19 +4,15 @@
 
 args<-commandArgs(trailingOnly=TRUE)
 
-if(args[1]=="-h"){
-print("This script converts SNP Association data into the right format for VEGAS.
-The arguments you need are:
-1. The file name for the SNP association file
-2. The column number containing the SNP names (rs…)
-3. The column number containing the SNP case-control association p-value
-4. An output directory")
+if(as.character(args[1])=="-h"){
+print("This script converts SNP Association data into the right format for VEGAS. The arguments you need are: 1. The file name for the SNP association file (e.g., ./BASH1_SNPAssociationToSNPPvalues.R phs000182.pha002890.txt), 2. The column number containing the SNP names (rs…) (e.g., 1), 3. The column number containing the SNP case-control association p-value (e.g., 2), 4. An output directory (e.g., ./Output/). Example syntax:  ./BASH1_SNPAssociationToSNPPvalues.R phs000182.pha002890.txt 1 2 ./Output/")
+quit()
 }else{}
 
-SNPAssociationFileName<-args[1]
-ColumnNumberSNPNames<-args[2]
-ColumnNumberSNPpvalues<-args[3]
-OutputPath<-args[4]
+SNPAssociationFileName<-as.character(args[1])
+ColumnNumberSNPNames<-as.numeric(args[2])
+ColumnNumberSNPpvalues<-as.numeric(args[3])
+OutputPath<-as.character(args[4])
 
 #Reading in our SNP association data, extracting the correct columns, and outputting it into a new file.
 
