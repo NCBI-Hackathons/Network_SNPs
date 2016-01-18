4f110e0c441f86e7cfd51334bd160429ce7795ec
Ubuntu <eric_moyer@yahoo.com>
2016-01-18 18:19:55 +0000

    Change SNPAssociationToSNPPvalues to take filename as well as directory

--- BASH1_SNPAssociationToSNPPvalues.R
+++ BASH1_SNPAssociationToSNPPvalues.R
@@ -5,7 +5,7 @@
 args<-commandArgs(trailingOnly=TRUE)
 
 if(as.character(args[1])=="-h"){
-print("This script converts SNP Association data into the right format for VEGAS. The arguments you need are: 1. The file name for the SNP association file (e.g., ./BASH1_SNPAssociationToSNPPvalues.R phs000182.pha002890.txt), 2. The column number containing the SNP names (rs…) (e.g., 1), 3. The column number containing the SNP case-control association p-value (e.g., 2), 4. An output directory (e.g., ./Output/). Example syntax:  ./BASH1_SNPAssociationToSNPPvalues.R phs000182.pha002890.txt 1 2 ./Output/")
+print("This script converts SNP Association data into the right format for VEGAS. The arguments you need are: 1. The file name for the SNP association file (e.g., ./BASH1_SNPAssociationToSNPPvalues.R phs000182.pha002890.txt), 2. The column number containing the SNP names (rs…) (e.g., 1), 3. The column number containing the SNP case-control association p-value (e.g., 2), 4. An output filename (e.g., ./Output/my_file). Example syntax:  ./BASH1_SNPAssociationToSNPPvalues.R phs000182.pha002890.txt 1 2 ./Output/my_file")
 quit()
 }else{}
 
@@ -25,6 +25,6 @@ print("These are the columns that you have indicated include the SNP names and p
 head(SNPAssociationFile[,c(ColumnNumberSNPNames,ColumnNumberSNPpvalues)])
 
 #Note: We need to extract out only the columns of data including the SNP names and p-values, no header:
-write.table(SNPAssociationFile[,c(ColumnNumberSNPNames,ColumnNumberSNPpvalues)], paste(OutputPath, "TestData_OnlySNPs_pvalues.txt", sep=""), quote=F, sep="\t",  col.names=F, row.names=F)
+write.table(SNPAssociationFile[,c(ColumnNumberSNPNames,ColumnNumberSNPpvalues)], OutputPath, quote=F, sep="\t",  col.names=F, row.names=F)
 
 q()
